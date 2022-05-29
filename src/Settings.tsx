d9e27d56bfd75110ab5f1ea8229849a9dec42029
Momtchil Momtchev <momtchil@momtchev.com>
2022-05-30 01:56:36 +0200

    hide notifications settings on iPhone

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Dimensions, StyleSheet, View, Text } from 'react-native';
+import { Dimensions, StyleSheet, View, Text, Platform } from 'react-native';
 
 import { StatusBar } from 'expo-status-bar';
 import Constants from 'expo-constants';
@@ -24,51 +24,58 @@ const styles = StyleSheet.create({
 const modeLabel = () => ({ 'P': i18n.t('Paragliding'), 'H': i18n.t('Hang-gliding'), S: i18n.t('Sailplane') }) as Record<veliMode, string>;
 const heightLabel: Record<veliHeight, string> = { 'G': 'AGL', 'S': 'AMSL' };
 
-const translateSettings = (config: Config, update: () => void) => [
-    {
-        label: i18n.t('Mode'), type: 'enum', values: ['P', 'H', 'S'], display: (v: string) => modeLabel()[v as veliMode],
-        get: config.get.bind(config, '@mode', () => 'P'), set: (v) => {
-            update();
-            return config.setMode(v as veliMode);
-        }
-    },
-    {
-        label: i18n.t('Height'), type: 'enum', values: ['G', 'S'], display: (v: string) => heightLabel[v as veliHeight],
-        get: config.get.bind(config, '@height', () => 'S'), set: (v) => {
-            update();
-            return config.setHeight(v as veliHeight);
-        }
-    },
-    {
-        label: i18n.t('Notifications'), type: 'section', elements: [
-            {
-                label: 'AROME', type: 'boolean',
-                get: config.getNotificationsStatus.bind(config, 'AROME'),
-                set: (v) => config.set('@nAROME', v.toString())
-            },
-            {
-                label: 'ARPEGE', type: 'boolean',
-                get: config.getNotificationsStatus.bind(config, 'ARPEGE'),
-                set: (v) => config.set('@nARPEGE', v.toString())
-            },
-            {
-                label: 'ICON-D2', type: 'boolean',
-                get: config.getNotificationsStatus.bind(config, 'ICON-D2'),
-                set: (v) => config.set('@nICON-D2', v.toString())
-            },
-            {
-                label: 'ICON-EU', type: 'boolean',
-                get: config.getNotificationsStatus.bind(config, 'ICON-EU'),
-                set: (v) => config.set('@nICON-EU', v.toString())
-            },
-            {
-                label: 'GFS', type: 'boolean',
-                get: config.getNotificationsStatus.bind(config, 'GFS'),
-                set: (v) => config.set('@nGFS', v.toString())
+const translateSettings = (config: Config, update: () => void) => {
+    const settings = [
+        {
+            label: i18n.t('Mode'), type: 'enum', values: ['P', 'H', 'S'], display: (v: string) => modeLabel()[v as veliMode],
+            get: config.get.bind(config, '@mode', () => 'P'), set: (v) => {
+                update();
+                return config.setMode(v as veliMode);
             }
-        ]
-    },
-    {
+        },
+        {
+            label: i18n.t('Height'), type: 'enum', values: ['G', 'S'], display: (v: string) => heightLabel[v as veliHeight],
+            get: config.get.bind(config, '@height', () => 'S'), set: (v) => {
+                update();
+                return config.setHeight(v as veliHeight);
+            }
+        }
+    ] as SettingsElement[];
+
+    if (Platform.OS === 'android') {
+        settings.push(
+            {
+                label: i18n.t('Notifications'), type: 'section', elements: [
+                    {
+                        label: 'AROME', type: 'boolean',
+                        get: config.getNotificationsStatus.bind(config, 'AROME'),
+                        set: (v) => config.set('@nAROME', v.toString())
+                    },
+                    {
+                        label: 'ARPEGE', type: 'boolean',
+                        get: config.getNotificationsStatus.bind(config, 'ARPEGE'),
+                        set: (v) => config.set('@nARPEGE', v.toString())
+                    },
+                    {
+                        label: 'ICON-D2', type: 'boolean',
+                        get: config.getNotificationsStatus.bind(config, 'ICON-D2'),
+                        set: (v) => config.set('@nICON-D2', v.toString())
+                    },
+                    {
+                        label: 'ICON-EU', type: 'boolean',
+                        get: config.getNotificationsStatus.bind(config, 'ICON-EU'),
+                        set: (v) => config.set('@nICON-EU', v.toString())
+                    },
+                    {
+                        label: 'GFS', type: 'boolean',
+                        get: config.getNotificationsStatus.bind(config, 'GFS'),
+                        set: (v) => config.set('@nGFS', v.toString())
+                    }
+                ]
+            });
+    }
+
+    settings.push({
         label: (
             <Text
                 style={{
@@ -84,8 +91,10 @@ const translateSettings = (config: Config, update: () => void) => [
         ),
         type: 'section',
         elements: []
-    }
-] as SettingsElement[];
+    });
+
+    return settings;
+};
 
 export default function Settings(props: {
     config: Config;
